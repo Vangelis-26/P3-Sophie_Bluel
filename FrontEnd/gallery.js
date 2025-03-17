059e89804075950cf792538b2fccbcc16110d716
Vangelis-26 <mourier.matthieu@gmail.com>
2025-03-17 22:12:00 +0100

    Adapting comments for easier reading

--- FrontEnd/gallery.js
+++ FrontEnd/gallery.js
@@ -1,64 +1,77 @@
-//////////////////
-///  Gallery   ///
-//////////////////
+/////////////////////////////////////////////////////////////////////
+///                                                               ///
+///                         Gallery                               ///
+///                                                               ///
+/////////////////////////////////////////////////////////////////////
 
 /**
  * @typedef {object} Category
- * @property {number} id
- * @property {string} name
+ * @property {number} id - L'identifiant unique de la catégorie.
+ * @property {string} name - Le nom de la catégorie.
  */
 
 /**
- * @typedef {object} Works
- * @property {number} id
- * @property {string} title
- * @property {number} categoryId
- * @property {number} userId
- * @property {Category} category
+ * @typedef {object} Project
+ * @property {number} id - L'identifiant unique du projet.
+ * @property {string} title - Le titre du projet.
+ * @property {number} categoryId - L'identifiant de la catégorie à laquelle le projet appartient.
+ * @property {number} userId - L'identifiant de l'utilisateur qui a ajouté le projet.
+ * @property {Category} category - L'objet catégorie associé au projet.
+ * @property {string} imageUrl - L'URL de l'image du projet.
  */
 
 /**
- * Création de la structure HTML de la Gallery
- * @param {Works[]} data
+ * Crée et ajoute les éléments HTML de la galerie à partir des données de projets fournies.
+ * @param {Project[]} data - Le tableau d'objets Projets à afficher dans la galerie.
  */
-
 const gallery = document.querySelector(".gallery");
 
 export function buildGallery(data) {
+  // Itère sur chaque élément du tableau de données
   for (const item of data) {
+    // Crée les éléments HTML pour chaque projet
     const figure = document.createElement("figure");
     const img = document.createElement("img");
     const figcaption = document.createElement("figcaption");
 
+    // Configure les attributs et le contenu des éléments
     img.setAttribute("src", item.imageUrl);
     img.setAttribute("alt", item.title);
     figcaption.textContent = item.title;
 
+    // Ajoute les éléments à la galerie
     gallery.appendChild(figure);
     figure.appendChild(img);
     figure.appendChild(figcaption);
   }
 }
 
-///////////////////
-///   Filtres   ///
-///////////////////
+/////////////////////////////////////////////////////////////////////
+///                                                               ///
+///                         Filtres                               ///
+///                                                               ///
+/////////////////////////////////////////////////////////////////////
 
 /**
  * @typedef {object} Category
- * @property {number} id
- * @property {string} name
+ * @property {number} id - L'identifiant unique de la catégorie.
+ * @property {string} name - Le nom de la catégorie.
  */
 
 const filters = document.querySelector(".categories");
 
-// Création des filtres
+/**
+ * Crée et ajoute les boutons de filtres à partir des données de catégories fournies.
+ * @param {Category[]} data - Le tableau d'objets Category à utiliser pour créer les filtres.
+ */
 export function buildCategories(data) {
+  // Crée le bouton "Tout" et l'ajoute
   const btnAll = document.createElement("button");
   btnAll.textContent = "Tout";
   btnAll.id = "0";
   filters.appendChild(btnAll);
 
+  // Itère sur chaque catégorie pour créer un bouton de filtre
   for (const item of data) {
     const button = document.createElement("button");
 
@@ -69,17 +82,22 @@ export function buildCategories(data) {
   }
 }
 
-// Ecoute et application des filtres
-
+/**
+ * Ajoute un écouteur d'événements pour filtrer et afficher les projets en fonction du bouton cliqué.
+ * @param {Project[]} data - Le tableau d'objets Project à filtrer.
+ */
 export function categoriesFilters(data) {
+  // Ajoute un écouteur d'événements pour gérer les clics sur les boutons de filtres
   filters.addEventListener("click", (e) => {
     const btnId = e.target.id;
 
+    // Filtre les éléments en fonction de l'ID du bouton cliqué
     const filteredElements =
       btnId === "0"
-        ? data
-        : data.filter((item) => item.categoryId.toString() === btnId);
+        ? data // Si "Tout" est cliqué, affiche toutes les données
+        : data.filter((item) => item.categoryId.toString() === btnId); // Sinon, filtre par categoryId
 
+    // Vide la galerie et affiche les éléments filtrés
     gallery.innerHTML = "";
     buildGallery(filteredElements);
   });
