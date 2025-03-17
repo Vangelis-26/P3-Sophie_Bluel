dbe50881b56c3d9fb8e3746d9d140f933f6298d7
Vangelis-26 <mourier.matthieu@gmail.com>
2025-03-17 20:37:27 +0100

    Activate Buttons Filters

--- FrontEnd/main.js
+++ FrontEnd/main.js
@@ -1,15 +1,18 @@
 // Récupération de tous les travaux via l'API
 import { getDataCategories, getDataGallery } from "./fetch.js";
-import { buildCategories, buildGallery } from "./gallery.js";
+import { buildCategories, buildGallery, categoriesFilters } from "./gallery.js";
 
 async function main() {
-  const gallery = await getDataGallery();
-  buildGallery(gallery);
-  console.log("gallery", gallery);
+  const workdata = await getDataGallery();
+  buildGallery(workdata);
+  console.log("gallery", workdata);
 
   const filters = await getDataCategories();
   buildCategories(filters);
   console.log("filters", filters);
+
+  categoriesFilters(workdata);
+  console.log("categories", workdata);
 }
 
 main();
