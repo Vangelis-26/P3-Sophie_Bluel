ee8e641b913dcf154a11bb92c6c3846563896df0
Vangelis-26 <mourier.matthieu@gmail.com>
2025-03-20 02:07:57 +0100

    Rework Login and Add Logout

---
+++ FrontEnd/JS/logout.js
@@ -0,0 +1,21 @@
+// Gestion du lien Login/Logout
+
+document.addEventListener("DOMContentLoaded", () => {
+  const loginLogoutLink = document.getElementById("loginLogoutLink");
+  const token = sessionStorage.getItem("token"); // ou localStorage.getItem("token")
+  console.log(loginLogoutLink);
+  if (token) {
+    // Utilisateur connecté
+    loginLogoutLink.textContent = "logout";
+    loginLogoutLink.href = "#";
+    loginLogoutLink.addEventListener("click", (e) => {
+      e.preventDefault();
+      sessionStorage.removeItem("token"); // ou localStorage.removeItem("token")
+      window.location.reload(); // Recharge la page
+    });
+  } else {
+    // Utilisateur déconnecté
+    loginLogoutLink.textContent = "login";
+    loginLogoutLink.href = "login.html";
+  }
+});
