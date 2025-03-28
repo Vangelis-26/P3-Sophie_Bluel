07a25d123c77e2e2163ac7f05173addd388ab0f2
Vangelis-26 <mourier.matthieu@gmail.com>
2025-03-28 11:13:37 +0100

    Add Navigation Pages Modal

--- FrontEnd/JS/modal.js
+++ FrontEnd/JS/modal.js
@@ -5,38 +5,38 @@
 /////////////////////////////////////////////////////////////////////
 
 const modal = document.querySelector(".modal");
-const jsModalClose = document.querySelector(".js-modal-close");
-const jsModalStop = document.querySelector(".js-modal-stop");
+
+// Pages Modal
+const modalEditGallery = document.getElementById("editGallery");
+const modalAddWork = document.getElementById("addPicture");
+
+// Boutons Modal
+const modal2 = document.getElementById("addPhotos");
+const modalBack = document.querySelector(".js-modal-back");
+const jsModalClose = document.querySelectorAll(".js-modal-close");
 const focusables = Array.from(
   modal.querySelectorAll("button, a, input, textarea")
 );
 
-const stopPropagation = function (e) {
-  e.stopPropagation();
-};
+const stopPropagation = (e) => e.stopPropagation();
+
+modalEditGallery.addEventListener("click", stopPropagation);
+modalAddWork.addEventListener("click", stopPropagation);
 
 // Ouverture Modal
 const openModal = function (e) {
-  e.preventDefault();
   modal.style.display = null;
   modal.removeAttribute("aria-hidden");
   modal.setAttribute("aria-modal", "true");
   modal.addEventListener("click", closeModal);
-  jsModalClose.addEventListener("click", closeModal);
-  jsModalStop.addEventListener("click", stopPropagation);
 };
 
 // Fermeture Modal
 const closeModal = function (e) {
-  e.preventDefault();
-  window.setTimeout(() => {
-    modal.style.display = "none";
-  }, 500);
+  modal.style.display = "none";
   modal.setAttribute("aria-hidden", "inert");
   modal.removeAttribute("aria-modal");
   modal.removeEventListener("click", closeModal);
-  jsModalClose.removeEventListener("click", closeModal);
-  jsModalStop.removeEventListener("click", stopPropagation);
 };
 
 // Focus dans la Modal
@@ -58,12 +58,19 @@ const focusInModal = function (e) {
   console.log(index);
 };
 
+// Ecoute des boutons d'ouverture
 document.addEventListener("DOMContentLoaded", function () {
   document.querySelectorAll(".js-modal").forEach((link) => {
     link.addEventListener("click", openModal);
   });
 });
 
+// Ecoute des boutons de fermeture
+jsModalClose.forEach((closeButton) => {
+  closeButton.addEventListener("click", closeModal);
+});
+
+// Ecoute des touches clavier
 window.addEventListener("keydown", function (e) {
   if (e.key === "Escape" || e.key === "Esc") {
     closeModal(e);
@@ -130,5 +137,25 @@ export async function modalDeleteWork(trashId) {
     });
   } catch (error) {
     console.error(error.message);
+  }
+}
+
+/////////////////////////////////////////////////////////////////////
+///                                                               ///
+///                        Modal Switch                           ///
+///                                                               ///
+/////////////////////////////////////////////////////////////////////
+
+modalBack.addEventListener("click", modalSwitch);
+modal2.addEventListener("click", modalSwitch);
+
+function modalSwitch(e) {
+  e.preventDefault();
+  if (modalEditGallery.style.display !== "none") {
+    modalEditGallery.style.display = "none";
+    modalAddWork.style.display = "flex";
+  } else {
+    modalAddWork.style.display = "none";
+    modalEditGallery.style.display = "flex";
   }
 }
