d396c48e66d13e558588f90cef7cbeb26dfad0e7
Vangelis-26 <mourier.matthieu@gmail.com>
2025-04-07 14:55:07 +0200

    Fix Fetch

--- FrontEnd/JS/fetch.js
+++
@@ -1,8 +0,0 @@
-// Récupération des données depuis l'API
-export async function fetchData(url) {
-  const r = await fetch(url);
-  if (!r.ok) {
-    throw new Error(`Erreur HTTP ! Statut : ${r.status}`);
-  }
-  return r.json();
-}
